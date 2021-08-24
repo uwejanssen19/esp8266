abe18c5bf85e6436977825895f31221083320b8b
uwejanssen19 <uwe@janssen-muenchen.de>
2021-08-24 23:49:39 +0200

    fix boolean state stuff, set D5 to pullup INPUT

--- humSensor/hum/hum.ino
+++ humSensor/hum/hum.ino
@@ -4,8 +4,31 @@
 #include "SoilSensor.h"
 #include <ArduinoOTA.h>
 #include "TimUtil.h"
-// uncomment next line for hardware without adafruit bmpXX or bme280 sensor
-//#define _PROTOTYPE
+//  prototype hardware: hardware WITHOUT (no adafruit bmpXX nor bme280 sensor AND no pmode switch)  but *WITH* display : #define _PROTOTYPE
+//  final hardware: hardware WITH (either adafruit bmpXX or bme280 sensor AND pmode switch) but without display : #undef _PROTOTYPE
+#undef _PROTOTYPE
+
+#ifdef _PROTOTYPE
+#include <U8g2lib.h>
+
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C 
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+#endif
+
+constexpr const char* HOSTNAME = "UweSolar1";
+#define RUN_MODE_STR "runMode = "
+#define RUN_MODE_SLEEP (RUN_MODE_STR "SLEEP")
+#define RUN_MODE_PGM (RUN_MODE_STR "PGM")
+#define BOOLSTR(b) (b?RUN_MODE_SLEEP:RUN_MODE_PGM)
+#define SLEEPMODE true
+#define PMODE false
 
 int cnt;
 String msg;
@@ -16,15 +39,15 @@ int vcc_raw = 0;
 float vcc = 0.0;
 int rssi = 0;
 String ip = "";
-boolean pmode = false;
+boolean runMode = SLEEPMODE;
 void setupOTA();
-void prepMsg(boolean pmode, String& msg);
+void prepMsg(boolean runMode, String& msg);
 TimUtilBase timUtil;
 String timeString = "";
 #ifndef _PROTOTYPE
 float bmeTemp = -1;
 float bmePress = -1;
-#endif _PROTOTYPE
+#endif // END #ifndef _PROTOTYPE
 void printValues(); // test only
 
 void setup() {
@@ -32,7 +55,7 @@ void setup() {
   //logfln("%s %s", "in ", __FUNCTION__);
   wifiSetup();
   mqttSetup();
-  pinMode(D5, INPUT); // used for detecting programming mode via external switch
+  pinMode(D5, INPUT_PULLUP); // D5 used for run mode, default = open = HIGH = true = sleep 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);   // OFF
   cnt = 0;
@@ -41,7 +64,10 @@ void setup() {
   timUtil.init();
   //timUtil.showTime();
 #ifndef _PROTOTYPE
-  bme.begin();
+  Serial.println(bme.begin(0x76));
+#endif
+#ifdef _PROTOTYPE
+  u8g2.begin();
 #endif
   setupOTA();
 
@@ -56,7 +82,7 @@ void loop() {
 #ifndef _PROTOTYPE
   // use the printValues method for the appropiate sensor
   //printValues <Adafruit_BMP085>() ;
-  //printValues<int>();
+  //printValues();
   bmeTemp = bme.readTemperature();
   bmePress = bme.readPressure();
 #endif
@@ -64,8 +90,12 @@ void loop() {
   //delay(1000);              // wait for a second
   //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   //delay(5);
+#ifdef _PROTOTYPE
+  vcc_raw = ESP.getVcc();
+#else
   vcc_raw = analogRead(A0);
   vcc = vcc_raw * 4.2 / 1024;
+#endif
 
 //  logfln("%s %u", "vcc = ", vcc);
   delay(5);
@@ -87,11 +117,7 @@ void loop() {
   mqttPublish("UweSolar1/time/value", timeString.c_str());
   delay(5);
   hum = soilSensor.readSoilHum();
-  //logfln("%s %f", "publishing humidity = ", hum);
-  char soilhumbuf[8];
-  String(hum, 0).toCharArray(soilhumbuf, sizeof(soilhumbuf));
-  //logfln("%s %s", "const char * soilhum = ", soilhumbuf);
-  mqttPublish("UweSolar1/bme/hum", soilhumbuf);
+  mqttPublish("UweSolar1/bme/hum", String(hum, 0).c_str());
   delay(10);
 #ifndef _PROTOTYPE
   mqttPublish("UweSolar2/bme/temp", String(bmeTemp).c_str());
@@ -99,46 +125,91 @@ void loop() {
   mqttPublish("UweSolar2/bme/press", String(bmePress/100).c_str());
   delay(10);
 #endif
-  mqttPublish("UweSolar2/bme/hum", soilhumbuf);
+  mqttPublish("UweSolar2/bme/hum", String(hum, 0).c_str());
   delay(10);
   //logfln("status =  %s", msg.c_str());
+#ifdef _PROTOTYPE
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.sendBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+ //u8g2.setFont(u8g2_font_t0_22b_tr);	// choose a suitable font
+//  u8g2.drawStr(0, 11, "Hello v1.1");	
+//  u8g2.drawStr(0, 22, "from VS mic!");
 
-  pmode = digitalRead(D5); // check if pmode switch is set to programming mode
-  //logfln("pmode = %i", pmode);
-  prepMsg(pmode, msg);
+  // display IP address
+  IPAddress localIP = WiFi.localIP();
+  String ip = localIP.toString();
+
+  u8g2.drawStr(15, 11, HOSTNAME);
+  u8g2.drawStr(15, 22, String(hum, 0).c_str());
+  //// display IP address
+  u8g2.drawStr(15, 33, timeString.c_str());	// write something to the internal memory
+  //// display IP address
+  u8g2.drawStr(15, 44, (String("IP: ") + ip).c_str());
+  u8g2.drawStr(15, 55, (String(rssi)+String("dBm")).c_str());
+
+  u8g2.sendBuffer();
+
+#endif // END #ifdef _PROTOTYPE
+#ifndef _PROTOTYPE
+  runMode = digitalRead(D5); // check if pmode switch is set to programming mode
+#endif
+  logfln("%s", BOOLSTR(runMode));
+  prepMsg(runMode, msg);
+//  logfln("status = %s", msg.c_str());
   mqttPublish("UweSolar1/status", msg.c_str());
   delay(5);
   // wait before sleeping to let data out
   delay(5000);
-  if (pmode) {
+#ifndef _PROTOTYPE
+  if (runMode == SLEEPMODE) {
+	  logfln("%s", "SLEEP mode active");
 	  digitalWrite(LED_BUILTIN, HIGH);   // OFF
-	  system_deep_sleep_instant(8 * 1000 * 1000); // sleep x * 1000 * 1000 [secs]
+	  delay(1);
+	  system_deep_sleep_instant(1800 * 1000 * 1000); // sleep x * 1000 * 1000 [secs]
   }
   else {
+	  logfln("%s", "PROGRAMMING mode active");
 	  digitalWrite(LED_BUILTIN, LOW); // ON
+	  delay(1);
 	  while (true) { // enter programming mode
 		  delay(2000);
-		  pmode = digitalRead(D5); // check if pmode switch is set to programming mode
-		  Serial.print("pmode = ");
-		  Serial.println(pmode);
-		  if (pmode) {
-			  // external switch was set to non programming mode -> leave loop
-			  Serial.println("pmode stopped");
+		  runMode = digitalRead(D5); // check if D5 switch is set to programming mode (active LOW)
+		  logfln("waiting for to be set to sleep mode");
+		  // check if OTA programming loop can be left
+		  if (runMode == SLEEPMODE) {
+			  // external switch was set to non programming mode -> leave programming loop 
+			  Serial.println("set to sleepmode");
 			  break;
 		  }
-		  else {
+		  else {// runMode = false = PGM
 			  // continue programming mode 
 			  ArduinoOTA.handle();
 			  delay(1);
-			  //logfln("waiting for switch to be set to sleep, pmode = %b",pmode);
 		  }
 	  }
   }
+#else
+  delay(1000);
+  ArduinoOTA.handle();
+#endif // real hardware ?
 }
-void prepMsg(boolean pmode, String& msg) {
-	msg.reserve(70);
-	msg = "UweSolar1 ";
-	msg += "/ ";
-	msg += ip; msg += " "; msg += rssi; msg += "dBm "; msg += vcc;
-	msg += "V, pmode = "; msg += (pmode ? "sleeep" : "pgm");
+//void prepMsg(boolean pmode, String& msg) {
+//	msg.reserve(80);
+//	msg += "UweSolar1 ";
+//	msg += "/ ";
+//	msg += ip; msg += " "; msg += rssi; msg += "dBm "; msg += vcc;
+//	msg += "V, pmode = "; msg += (pmode ? "sleep" : "pgm");
+//}
+void prepMsg(boolean runMode, String& msg) {
+	msg = 
+		String(HOSTNAME) + 
+		" / "+ 
+		ip + 
+		" " + 
+		rssi + 
+		"dBm " + 
+		vcc + 
+		"V, pmode = " + 
+		(runMode ? "sleep" : "pgm");
 }
